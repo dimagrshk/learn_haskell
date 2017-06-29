775116f453bf898edec431c93ebd7ea520821044
Dmytro Grishko <midnights44444@gmail.com>
2017-06-30 00:18:07 +0300

    some functions for learn haskell

--- pattern.hs
+++ pattern.hs
@@ -61,8 +61,57 @@ cylinder r h = let sideArea = 2 * pi * r * h
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "Empty list"
 maximum' [x] = x
-maximum' (x:xs)
-    | x > maxTail = x
-    | otherwise = maxTail
-    where maxTail = maximum' xs
+maximum' (x:xs) = max' x (maximum' xs)
 
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x 
+    | n <= 0 = []
+    | otherwise = x:replicate' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False 
+elem' a (x:xs) 
+    | a == x = True
+    | otherwise = a `elem` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = 
+    let
+        smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in
+        smallerSorted ++ [x] ++ biggerSorted
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = [] 
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y:zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)  
+flip' f = g  
+    where g x y = f y x  
