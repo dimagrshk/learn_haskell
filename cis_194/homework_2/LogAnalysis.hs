d713076286c2c18cc12c90d7189496483dff97df
Dmytro Grishko <midnights44444@gmail.com>
2017-07-08 01:34:21 +0300

    added parser for errors

--- cis_194/homework_2/LogAnalysis.hs
+++ cis_194/homework_2/LogAnalysis.hs
@@ -1,11 +1,24 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
-import Log
+import Data.String.Utils
+
+--import Log
+data MessegeType = Info | Warning | Error Int deriving (Show, Eq)
+
+type TimeStamp = Int
+
+data LogMessege = LogMessege MessegeType TimeStamp String | Uknown String deriving (Show, Eq)
 
 parseMessege :: String -> LogMessege
-parseMessege [] = Unkown "S"
+parseMessege [] = Uknown ""
 parseMessege (x:xs) 
-    | x == "I" =    
-    | x == "W" = 
-    | x == "E" =  
+    | x == 'I' = LogMessege Info (read code :: Int) inform
+    | x == 'W' = LogMessege Warning (read code :: Int) inform
+    | x == 'E' = LogMessege (Error (read code :: Int)) (read codeOfError :: Int) informOfError
+    | otherwise = Uknown ""
+    where inform = drop (2+(length code)) xs
+          code = head (splitWs xs)
+          codeOfError = head (drop 1 (splitWs xs))
+          informOfError = drop (3 + (length code) + (length codeOfError)) xs
+
